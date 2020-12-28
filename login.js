3c9c2ab70136038ec3469abb83e11a8865264002
Twinkle0057 <47236489+Twinkle0057@users.noreply.github.com>
2020-12-28 17:13:30 +0530

    commit

---
+++ login.js
@@ -0,0 +1,12 @@
+function onClickLogin(){
+    window.location.pathname = 'D:/WorkSpace/Addverb/main.html';
+}
+function onClickTeacher(){
+    window.location.pathname = 'D:/WorkSpace/Addverb/teacherDashboard.html';
+}
+function onClickStudent(){
+    window.location.pathname = 'D:/WorkSpace/Addverb/studentDashboard.html';
+}
+function onClickCoun(){
+    window.location.pathname = 'D:/WorkSpace/Addverb/counDashboard.html';
+}
